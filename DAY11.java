f21e46ee90f9201c881a5d42e470358e2c717632
IMRAN <88793320+Imranhub786@users.noreply.github.com>
2023-05-10 20:08:11 +0530

    Create DAY11.java

---
+++ DAY11.java
@@ -0,0 +1,49 @@
+/*Day 11 coding Statement:  Write a program to find Fibonacci series up to n
+
+Description
+
+Fibonacci series is a special series where nth term is the sum of previous two terms in the series. The series starts with 0 and 1 as the first and second term of the series respectively.
+
+Here you need to get the value for nth term from user and then print Fibonacci series containing n terms.
+
+Input
+
+5
+
+Output
+
+0,1,1,2,3
+
+Input
+
+8
+
+Output
+
+0,1,1,2,3,5,8,13
+  */
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + ", ");
+        }
+        
+        scanner.close();
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}

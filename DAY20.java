9de5fd0731b2a720485a52222dba084f0fcf9b79
IMRAN <88793320+Imranhub786@users.noreply.github.com>
2023-05-19 23:40:14 +0530

    Create DAY20.java

---
+++ DAY20.java
@@ -0,0 +1,54 @@
+/*Day 20 coding Statement : Write a program to identify if the number is Prime number or not
+
+Description
+
+Get a number as input from the user and check whether that number is prime or not.
+
+A prime number is a number with factors as 1 and that number itself.
+
+Input
+
+1
+
+Output
+
+1 is not a prime number
+
+Input
+
+5
+
+Output
+
+5 is a prime number 
+  */
+import java.util.Scanner;
+
+class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        } else {
+            System.out.println(number + " is not a prime number");
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        // Check for divisibility from 2 to the square root of the number
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false; // Number is divisible, hence not prime
+            }
+        }
+
+        return true; // Number is prime
+    }
+}
